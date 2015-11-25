622b336c525a3bfb70b39555ee495b7db9bbc673
Chonoo <johan.byttner@gmail.com>
2015-11-25 16:33:24 +0000

    Adding all compulsory functions, normed and compiled

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jbyttner <jbyttner@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 15:37:41 by jbyttner          #+#    #+#             */
-/*   Updated: 2015/11/25 15:41:27 by jbyttner         ###   ########.fr       */
+/*   Updated: 2015/11/25 16:31:07 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,5 +57,33 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 
 char	*ft_strrchr(const char *s, int c);
+
+char	*ft_strstr(const char *haystack, const char *needle);
+
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+
+int		ft_strcmp(const char *s1, const char *s2);
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+int		ft_atoi(const char *nptr);
+
+int		ft_islower(int c);
+
+int		ft_isupper(int c);
+
+int		ft_isalpha(int c);
+
+int		ft_isdigit(int c);
+
+int		ft_isalnum(int c);
+
+int		ft_isascii(int c);
+
+int		ft_isprint(int c);
+
+int		ft_toupper(int c);
+
+int		ft_tolower(int c);
 
 #endif
