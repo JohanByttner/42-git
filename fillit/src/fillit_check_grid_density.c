10f5c03acca62aca69555873232e9eb1a8f2c602
Johan Byttner <johan.byttner@gmail.com>
2016-01-28 13:27:44 +0000

    Added a mostly working iterator

---
+++ fillit/src/fillit_check_grid_density.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit_check_grid_density.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbyttner <jbyttner@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/25 18:28:55 by jbyttner          #+#    #+#             */
+/*   Updated: 2016/01/28 13:25:16 by jbyttner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+static  void		fillit_reset_grid(char *grid, int size)
+{
+	int		i;
+
+	i = 0;
+	while (i++ < size * size)
+		if (grid[i] == '%')
+			grid[i] = '.';
+}
+
+static int		fillit_expand_grid(char *grid, int start, int size)
+{
+	int		acc;
+	int		tmp;
+
+	acc = 0;
+	if (start > size * size)
+		acc = 0;
+	else if (grid[start] == '.')
+	{
+		acc = 1;
+		grid[start] = '%';
+		tmp = 0;
+		/*if ((start + 1) % size && (tmp = fillit_expand_grid(grid, start + 1, size)) == -1)
+			return (-1);
+		acc += tmp;
+		if (1)
+		{
+			if ((tmp = fillit_expand_grid(grid, start + size, size)) == -1)
+				return (-1);
+			acc += tmp;
+		}
+		if ((start - 1) % size)
+		{
+			if ((tmp = fillit_expand_grid(grid, start - 1, size)) == -1)
+				return (-1);
+			acc += tmp;
+		}*/
+		if ((start) % size)
+		{
+			tmp = fillit_expand_grid(grid, start + 1, size);
+			acc += tmp;
+		}
+		tmp = fillit_expand_grid(grid, start + size, size);
+		acc += tmp;
+		if ((start - 1) % size)
+		{
+			tmp = fillit_expand_grid(grid, start - 1, size);
+			acc += tmp;
+		}
+	}
+	else if (grid[start] == '%')
+		return (0); //(-1);
+	else
+		acc = 0;
+	return (acc);
+}
+
+inline int		fillit_check_grid_density(char *grid)
+{
+	int		size[2];
+	int		i;
+	int		acc;
+
+	size[0] = (int)grid[0];
+	size[1] = (int)grid[1 + size[0] * size[0]];
+	i = 0;
+	while (i++ < size[0] * size[0])
+	{
+		if (grid[i] == '.')
+		{
+			if ((acc = fillit_expand_grid(grid, i, size[0])) == -1)
+				continue ;
+			if (acc >= 4)
+				acc = 0;
+			if ((size[1] -= acc) < 0)
+			{
+				//ft_putstr("\nReset printing grid\n");
+				///fillit_print_grid(grid);
+				fillit_reset_grid(grid, size[0]);
+				//fillit_print_grid(grid);
+				return (0);
+			}
+		}
+	}
+	fillit_reset_grid(grid, size[0]);
+	return (1);
+}
--- fillit/src/fillit_solve_grid.c
+++ fillit/src/fillit_solve_grid.c
@@ -6,25 +6,55 @@
 /*   By: jbyttner <jbyttner@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/21 23:21:14 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/01/25 14:39:45 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/01/28 13:26:16 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+int			fillit_check_grid_density(char *grid)
+;/*{
+	int		size;
+	int		space;
+	int		i;
+	int		acc;
+
+	size = (int)grid[0];
+	space = (int)grid[1 + size * size];
+	acc = 0;
+	i = 0;
+	while (i++ < size * size)
+	{
+		if (grid[i] != '.')
+		{
+			space -= acc;
+			acc = 0;
+		}
+		else
+			acc++;
+		if (space < 0)
+		{
+			ft_putnbr(space);
+			ft_putstr("size\n");
+			return (0);
+	}
+	}
+	return (1);
+}*/
+
 static inline int	fillit_set_pos(int offs[], int pos[], int *shape, int size)
 {
 	offs[0] = pos[0] + shape[0];
 	offs[1] = pos[1] + shape[1];
-	ft_putstr("Offs : "); ft_putnbr(offs[0]); ft_putchar('|'); ft_putnbr(offs[1]);
-	ft_putstr(" size : "); ft_putnbr(size); ft_putstr("\n");
+	//ft_putstr("Offs : "); ft_putnbr(offs[0]); ft_putchar('|'); ft_putnbr(offs[1]);
+	//ft_putstr(" size : "); ft_putnbr(size); ft_putstr("\n");
 	if (offs[0] < 0 || size <= offs[0])
 		return (0);
 	else if (offs[1] < 0 || size <= offs[1])
 		return (0);
 	else
 	{
-		ft_putstr("Success!\n");
+		//ft_putstr("Success!\n");
 		return (1);
 	}
 }
@@ -34,10 +64,11 @@ int		fillit_place_shape(int **shapes, char *grid, int pos[], int count)
 	int		offs[2];
 	int		size;
 	int		grid_pos;
+	int		r;
 
-	ft_putstr("Count :");
-	ft_putnbr(count);
-	ft_putchar('\n');
+	//ft_putstr("Count :");
+	//ft_putnbr(count);
+	//ft_putchar('\n');
 	size = (int)grid[0];
 	if (!(count % 4))
 	{
@@ -53,14 +84,20 @@ int		fillit_place_shape(int **shapes, char *grid, int pos[], int count)
 			count++;
 			if (count % 4)
 			{
-				ft_putstr("I am placing shape\n");
-				if (fillit_place_shape(shapes, grid, pos, count))
+				//ft_putstr("I am placing shape\n");
+				if ((r = fillit_place_shape(shapes, grid, pos, count)) == 1)
 					return (1);
-				ft_putstr("I failed\n");
+				//else if (r == -1)
+				//	return (-1);
+				//ft_putstr("I failed\n");
 			}
 			else
-				if (fillit_find_space(shapes + 1, grid, count))
+			{
+				if ((r = fillit_find_space(shapes + 1, grid, count)) == 1)
 					return (1);
+				//else if (r == -1)
+				//	return (-1);
+			}
 			grid[grid_pos] = '.';
 		}
 	}
@@ -72,24 +109,35 @@ int			fillit_find_space(int **shapes, char *grid, int count)
 	int		pos[2];
 	int		size;
 	int		i;
+	int		r;
 
 	if (*shapes)
 	{
+		if (!(fillit_check_grid_density(grid)))
+			return (-1);
 		i = 0;
 		size = (int)grid[0];
 		while (i < size * size)
 		{
+		/*ft_putstr("I ");
+		ft_putnbr(i);
+		ft_putchar('|');
+		ft_putnbr(size);
+		ft_putstr("\n");*/
 			if (grid[i + 1] == '.')
 			{
-				ft_putstr("Placing");
 				pos[0] = i / size;
 				pos[1] = i % size;
-				ft_putnbr(pos[0]);
-				ft_putchar('|');
-				ft_putnbr(pos[1]);
-				ft_putchar('\n');
-				if (fillit_place_shape(shapes, grid, pos, count))
+				if ((r = fillit_place_shape(shapes, grid, pos, count) == 1))
 					return (1);
+				else if (r == -1)
+					return (0);
+				else if (count == ('T' - 'A') * 4)
+				{
+					ft_putstr("\nMain printing grid\n");
+					fillit_print_grid(grid);
+					ft_putchar('\n');
+				}
 			}
 			i++;
 		}
@@ -111,25 +159,34 @@ char		*fillit_validate_grid(int **shapes)
 	char	*grid;
 	char	size;
 	int		i;
+	int		num_figures;
 
-	size = 2;
-	grid = ft_memalloc(sizeof(char) * size * size + 1);
+	num_figures = 0;
+	size = 0;
+	while(shapes[num_figures])
+		num_figures++;
+	//num_figures--;
+	num_figures *= 4;
+	while (size * size < num_figures)
+		size++;
+	grid = ft_memalloc(sizeof(char) * (size * size + 2));
 	grid[0] = size;
+	i = 0;
+	while (i++ < size * size)
+		grid[i] = '.';
+	grid[size * size + 1] = size * size - num_figures;
+	ft_putnbr(size);
 	while (!(fillit_find_space(shapes, grid, 0)))
 	{
+		ft_putstr("\nIncreasing size\n");
 		size++;
 		free(grid);
-		grid = ft_memalloc(sizeof(char) * size * size + 1);
+		grid = ft_memalloc(sizeof(char) * size * size + 2);
 		i = 0;
-		while (i < size * size)
-		{
-			grid[i + 1] = '.';
-			i++;
-		}
+		while (i++ < size * size)
+			grid[i] = '.';
 		grid[0] = size;
-		ft_putnbr(size);
-		ft_putstr(" Loop\n");
-		//usleep(100000);
+		grid[size * size + 1] = size * size - num_figures;
 	}
 	return (grid);
 }
