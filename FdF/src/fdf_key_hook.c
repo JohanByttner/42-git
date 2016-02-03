d07e9d93477dd838cf8f44253e1d6137f89b1eaf
Johan Byttner <johan.byttner@gmail.com>
2016-02-03 23:09:40 +0000

    Adding back functionality in linux distro, fixing broken keysyms

--- FdF/src/fdf_key_hook.c
+++ FdF/src/fdf_key_hook.c
@@ -6,7 +6,7 @@
 /*   By: jbyttner <jbyttner@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/02 17:44:18 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/02/03 20:59:47 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/02/03 23:07:21 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	fdf_key_hook_two(int keycode, t_mlx_camera *camera)
 	double		angle;
 
 	angle = 0;
-	if (keycode == XK_Z || keycode == XK_C)
+	if (keycode == XKC_Z || keycode == XKC_C)
 	{
-		angle = (keycode == XK_Z ? -FDF_ANGLE_STEP : FDF_ANGLE_STEP);
+		angle = (keycode == XKC_Z ? -FDF_ANGLE_STEP : FDF_ANGLE_STEP);
 		mlx_rotatek_camera(camera, angle);
 	}
-	else if (keycode == XK_Q || keycode == XK_E)
+	else if (keycode == XKC_Q || keycode == XKC_E)
 	{
-		angle = (keycode == XK_Q ? -FDF_ANGLE_STEP : FDF_ANGLE_STEP);
+		angle = (keycode == XKC_Q ? -FDF_ANGLE_STEP : FDF_ANGLE_STEP);
 		mlx_rotatei_camera(camera, angle);
 	}
 	else
@@ -39,19 +39,19 @@ int	fdf_key_hook_one(int keycode, t_mlx_camera *camera)
 	t_3dpoint		p;
 
 	p = (t_3dpoint){ 0, 0, 0 };
-	if (keycode == XK_A || keycode == XK_D)
+	if (keycode == XKC_A || keycode == XKC_D)
 	{
-		p.i = (keycode == XK_A ? -FDF_MOVE_STEP : FDF_MOVE_STEP);
+		p.i = (keycode == XKC_A ? -FDF_MOVE_STEP : FDF_MOVE_STEP);
 		mlx_translate_camera(camera, &p);
 	}
-	else if (keycode == XK_W || keycode == XK_S)
+	else if (keycode == XKC_W || keycode == XKC_S)
 	{
-		p.j = (keycode == XK_W ? -FDF_MOVE_STEP : FDF_MOVE_STEP);
+		p.j = (keycode == XKC_W ? -FDF_MOVE_STEP : FDF_MOVE_STEP);
 		mlx_translate_camera(camera, &p);
 	}
-	else if (keycode == XK_PLUS || keycode == XK_MINUS)
+	else if (keycode == XKC_KP_ADD || keycode == XKC_KP_SUBTRACT)
 	{
-		p.k = (keycode == XK_PLUS ? FDF_MOVE_STEP : -FDF_MOVE_STEP);
+		p.k = (keycode == XKC_KP_ADD ? FDF_MOVE_STEP : -FDF_MOVE_STEP);
 		mlx_translate_camera(camera, &p);
 	}
 	else
@@ -65,7 +65,7 @@ int	fdf_key_hook(int keycode, void *param)
 
 	if (!(camera = mlx_get_image(0)->world->camera))
 		return (0);
-	else if (keycode == XK_ESCAPE)
+	else if (keycode == XKC_ESCAPE)
 		fdf_exit(0 && param);
 	else if (fdf_key_hook_one(keycode, camera))
 	{
