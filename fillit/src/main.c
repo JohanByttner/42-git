2a53f7c3b673fb8621ae1dc00806504a623e71ea
Johan BYTTNER <jbyttner@e1r10p8.42.fr>
2016-02-09 17:24:54 +0100

    Updating fillit to display error in case of no file or no valid file

--- fillit/src/main.c
+++ fillit/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jbyttner <jbyttner@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 12:27:10 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/02/08 16:26:05 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/02/09 17:23:43 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	main(int argc, char **argv)
 	int		**ptr;
 
 	if (argc == 2)
+	{
 		if ((fd = open(argv[1], O_RDONLY)) >= 0)
 		{
 			ptr = fillit_read_file(fd);
@@ -27,5 +28,10 @@ int	main(int argc, char **argv)
 			fillit_validate_figures(ptr);
 			fillit_print_grid(fillit_solve_grid(ptr));
 		}
+		else
+			fillit_exit(-1);
+	}
+	else
+		fillit_exit(-1);
 	return (0);
 }
